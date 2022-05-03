09a32c684293524f8a9b177aca836a4fb508f140
TiagoDiass <tiago.costadiasss@gmail.com>
2022-05-03 00:07:18 -0300

    test: add Header tests

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,8 +1,67 @@
 import Header from './Header';
 import { renderWithTheme } from 'utils/test-utils';
+import { screen } from '@testing-library/react';
+import { renderHook, RenderResult } from '@testing-library/react-hooks';
+import useCartStore, { CartStore } from 'store/cart/cart.store';
 
 describe('Component: Header', () => {
+  let result: RenderResult<CartStore>;
+
+  beforeEach(() => {
+    result = renderHook(() => useCartStore()).result;
+  });
+
   it('should render correctly', () => {
     renderWithTheme(<Header />);
+
+    expect(screen.getByRole('img', { name: 'Rocketshoes' })).toBeInTheDocument();
+    expect(screen.getByText('My cart')).toBeInTheDocument();
+  });
+
+  it('should render products amount correctly when amount is 0', () => {
+    result.current.state.products = [];
+
+    renderWithTheme(<Header />);
+
+    expect(screen.getByTestId('products-amount')).toHaveTextContent('No items');
+  });
+
+  it('should render products amount correctly when amount is 1', () => {
+    result.current.state.products = [
+      {
+        id: 1,
+        title: 'Nike Airmax 90',
+        price: 249.9,
+        image: 'https://example-image.com',
+        amount: 1
+      }
+    ];
+
+    renderWithTheme(<Header />);
+
+    expect(screen.getByTestId('products-amount')).toHaveTextContent('1 item');
+  });
+
+  it('should render products amount correctly when amount is greater than 1', () => {
+    result.current.state.products = [
+      {
+        id: 1,
+        title: 'Nike Airmax 90',
+        price: 249.9,
+        image: 'https://example-image.com',
+        amount: 1
+      },
+      {
+        id: 2,
+        title: 'Adidas Superstars',
+        price: 199.9,
+        image: 'https://example-image.com',
+        amount: 1
+      }
+    ];
+
+    renderWithTheme(<Header />);
+
+    expect(screen.getByTestId('products-amount')).toHaveTextContent('2 items');
   });
 });
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,16 +6,16 @@ import useCartStore from 'store/cart/cart.store';
 function Header() {
   const productsOnCart = useCartStore((store) => store.state.products);
 
-  // const getProductsAmountLabel = () => {
-  //   switch (productsOnCart.length) {
-  //     case 0:
-  //       return 'Nenhum item';
-  //     case 1:
-  //       return '1 item';
-  //     default:
-  //       return `${productsOnCart.length} itens`;
-  //   }
-  // };
+  const getProductsAmountLabel = () => {
+    switch (productsOnCart.length) {
+      case 0:
+        return 'No items';
+      case 1:
+        return '1 item';
+      default:
+        return `${productsOnCart.length} items`;
+    }
+  };
 
   return (
     <S.Wrapper>
@@ -25,10 +25,8 @@ function Header() {
 
       <S.Cart>
         <div>
-          <strong>Meu carrinho</strong>
-          <span>
-            {productsOnCart.length > 0 ? `${productsOnCart.length} itens` : 'Nenhum item'}
-          </span>
+          <strong>My cart</strong>
+          <span data-testid='products-amount'>{getProductsAmountLabel()}</span>
         </div>
         <MdShoppingBasket size={36} color='#FFF' />
       </S.Cart>

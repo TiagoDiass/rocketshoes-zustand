6b29eff915f409c6350370a3903327ce15eff9fe
TiagoDiass <tiago.costadiasss@gmail.com>
2022-05-22 23:00:37 -0300

    chore: add comment

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+// index to export all component, so I can import all I want from 'components' folder
 export { default as Main } from './Main/Main';
 export { default as Header } from './Header/Header';
 export { default as ProductCard } from './ProductCard/ProductCard';
